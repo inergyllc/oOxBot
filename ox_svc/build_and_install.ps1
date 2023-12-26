34beaf3ba5500dbbfd2f0c5f45caeb7af5029a73
inergyllc <amram.dworkin@inergyllc.com>
2023-12-25 21:59:35 -0500

    ox_svc 0.1.1

--- ox_svc/build_and_install.ps1
+++ ox_svc/build_and_install.ps1
@@ -1,5 +1,74 @@
 # build_and_install.ps1
 
+function Update-PyModuleVersion {
+    param(
+        [string]$SetupFilePath = ".\setup.py"
+    )
+
+    # Read setup.py content
+    $content = Get-Content $SetupFilePath
+
+    # Find the line containing the version number
+    $versionLine = $content | Where-Object { $_ -match "version=" }
+
+    # Extract the current version number
+    $currentVersion = [regex]::Match($versionLine, '(?<=")(.*?)(?=")').Value
+    $versionParts = $currentVersion.Split('.')
+
+    $major = [int]$versionParts[0]
+    $mid = [int]$versionParts[1]
+    $minor = [int]$versionParts[2]
+
+    # Apply version update rules
+    if ($minor -lt 9) {
+        $minor++
+    } elseif ($mid -lt 9) {
+        $minor = 0
+        $mid++
+    } else {
+        $minor = 0
+        $mid = 0
+        $major++
+    }
+
+    # Construct new version number
+    $newVersion = "$major.$mid.$minor"
+
+    # Replace the old version number with the new one in the content
+    $newContent = $content -replace "version=""$currentVersion""", "version=""$newVersion"""
+
+    # Write the updated content back into setup.py
+    Set-Content $SetupFilePath $newContent
+
+    Write-Host "Updated version to $newVersion"
+}
+
+function Remove-OldPyModule {
+    param(
+        [string]$moduleName
+    )
+
+    # Get list of outdated Python packages
+    $outdatedPackages = pip list --outdated --format=freeze | Where-Object { $_ -match "^$moduleName==" }
+
+    foreach ($package in $outdatedPackages) {
+        # Extract the package name and version
+        $parts = $package -split "=="
+        $name = $parts[0]
+        $version = $parts[1]
+
+        # Uninstall the old version
+        pip uninstall -y $name
+        Write-Host "Removed $name version $version"
+    }
+}
+
+# Go to the root directory for the github code
+cd "E:\oxai\src\OxBot" 
+
+# Update the version number in setup.py
+Update-PyModuleVersion -SetupFilePath ".\ox_svc\setup.py"
+
 # Navigate to the directory containing setup.py
 cd "E:\oxai\src\OxBot\ox_svc" 
 
@@ -13,3 +82,13 @@ python setup.py sdist bdist_wheel
 # Adjust the following line to match the naming pattern of your .whl file or find the .whl file automatically
 $wheelFile = Get-ChildItem -Path .\dist\ -Filter ox_svc*.whl | Select-Object -First 1
 pip install --upgrade $wheelFile.FullName
+
+# Navigate back to the root directory
+Remove-OldPyModule -moduleName "ox-svc"
+
+git add .
+
+$commitMessage = Read-Host -Prompt "Enter commit message"
+git commit -m "$commitMessage"
+
+git push origin master
--- ox_svc/setup.py
+++ ox_svc/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ox_svc",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     description="Service module for OxBot applications",
     author="Amram E. Dworkin",
