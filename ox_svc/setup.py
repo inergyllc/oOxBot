1186c2c68258ad1022ca0c74c89be0de26b41f2c
inergyllc <amram.dworkin@inergyllc.com>
2023-12-26 00:50:55 -0500

    ox_svc updated to version 0.3.3

--- ox_svc/setup.py
+++ ox_svc/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ox_svc",
-    version="0.3.2",
+    version="0.3.3",
     packages=find_packages(),
     description="Service module for OxBot applications",
     author="Amram E. Dworkin",
