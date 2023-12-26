dac50e53e2cf85811e603afcdf6b9f3566774f04
inergyllc <amram.dworkin@inergyllc.com>
2023-12-26 00:14:35 -0500

    ox_svc 0.2.3

--- OxBot/OxBot.py
+++ OxBot/OxBot.py
@@ -1,1 +1,14 @@
+from ox_svc import ListingsToExcel
 
+
+# Create excel listing
+listings_json_file_path = "E:/oxai/data/current/listings/listing_master_mirror.json"
+excluded_fields = [
+    'owconnect_category_id',
+    'owconnect_category_name',
+    'listing_id',
+    'sequence',
+    'logo: img/corp/generic-corp-logo-13.png'
+]
+# listings_to_excel = ListingsToExcel("E:/oxai/data/current/listings/listing_master_mirror.json", excluded_fields=excluded_fields)
+# listings_to_excel.process_listings()
--- ox_svc/setup.py
+++ ox_svc/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ox_svc",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     description="Service module for OxBot applications",
     author="Amram E. Dworkin",
